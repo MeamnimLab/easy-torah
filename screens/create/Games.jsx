4d62adb7ab0591388f28005b496f4f5b83304894
MeamnimLab <meamnimidf@gmail.com>
2024-11-25 16:19:47 +0200

    create vocabulary start

--- screens/create/Games.jsx
+++ screens/create/Games.jsx
@@ -107,15 +107,7 @@ const AllSubLevelGamesPage = ({ route, navigation }) => {
       <GameOptionsListModal
         isVisible={isModalVisible}
         onClose={() => setModalVisible(false)}
-<<<<<<< HEAD
-<<<<<<< HEAD
-        onCardPress={()=> console.log("card press")}
-=======
         onCardPress={()=> navigation.navigate("CreateVocabulary")}
->>>>>>> eff26ddf9b85d903666bb9aca456cecced911225
-=======
-        onCardPress={()=> navigation.navigate("CreateVocabulary")}
->>>>>>> eff26ddf9b85d903666bb9aca456cecced911225
       />
     </View>
   );
